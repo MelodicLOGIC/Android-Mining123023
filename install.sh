b17b8a9aa4e412b145f0b1ae35bef867c06ee386
MelodicLOGIC <134335844+MelodicLOGIC@users.noreply.github.com>
2023-12-30 13:38:50 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -48,7 +48,7 @@ then
     fi
   done
 fi
-wget https://raw.githubusercontent.com/Oink70/Android-Mining/main/config.json -P ~/ccminer
+wget https://github.com/MelodicLOGIC/Android-Mining123023/blob/main/config.json -P ~/ccminer
 
 if [ -f ~/ccminer/ccminer ]
 then
